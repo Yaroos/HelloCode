f4658d5c4526f4980752d6b34cad5b1153eb8338
Yaroslav <rotochenkov@icloud.com>
2023-01-14 22:08:44 +0300

    If and Else 005

---
+++ 005_ifandelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя польователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "марина")
+{
+    Console.WriteLine("Ура, это же МАРИНА!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
